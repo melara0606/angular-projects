44dfd0798cc25b3136bce8be92cc73ed8f7bcba2
unknown <melara0606@gmail.com>
2019-05-19 20:44:58 -0600

    Modificaciones necesarias para implementar entity store

--- src/app/app.reducers.ts
+++ src/app/app.reducers.ts
@@ -1,11 +1,7 @@
 import { ActionReducerMap } from '@ngrx/store';
-import { LessonsState } from './store-app/app.store';
-import { createReducers } from './store-app/app.reducers';
 
 export interface AppState {
-  items: LessonsState;
 }
 
 export const appReducers: ActionReducerMap<AppState> = {
-  items: createReducers
 };
